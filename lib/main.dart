d5cefab59d3c769b75f02a814cd6be1bf232b9dd
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-10-07 19:26:17 +0200

    ✨ Add onTap functionality to favorite names

--- lib/main.dart
+++ lib/main.dart
@@ -35,17 +35,14 @@ class _RandomWordsState extends State<RandomWords> {
 
   Widget _buildSuggestions() {
     return ListView.builder(
-        
         padding: const EdgeInsets.all(16),
-        
         itemBuilder: (BuildContext _context, int i) {
-          
           if (i.isOdd) {
             return Divider();
           }
 
           final int index = i ~/ 2;
-          
+
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
@@ -56,15 +53,21 @@ class _RandomWordsState extends State<RandomWords> {
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save'
-      )
-    );
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save'),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        });
   }
 }
